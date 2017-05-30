0faed7faec67c682243609d3954e8f17622fccf6
amit chaudhary <amit2cha@hotmail.com>
2017-05-30 14:53:14 +0530

    Added Add a GIg Page

---
+++ zichub/Controllers/GigsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using zichub.Models;
+using zichub.ViewModels;
+
+namespace zichub.Controllers
+{
+    public class GigsController : Controller
+    {
+        private ApplicationDbContext _context;
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Gigs/Create
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
