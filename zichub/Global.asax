224703a61c69e69d64956bc1e407e6d86c716492
amit chaudhary <amit2cha@hotmail.com>
2017-05-30 07:22:04 +0530

    Add project files.

---
+++ zichub/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="zichub.MvcApplication" Language="C#" %>
---
+++ zichub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(zichub.Startup))]
+namespace zichub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
