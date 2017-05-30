d91c1cb6d7f003c7babcccc29e72ed3dc9b45e26
amit chaudhary <amit2cha@hotmail.com>
2017-05-30 10:13:00 +0530

    Added Genres Data

---
+++ zichub/Migrations/201705300437410_PopulateGenreTable.cs
@@ -0,0 +1,22 @@
+namespace zichub.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class PopulateGenreTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO Genres (Id, Name) Values (1, 'Jazz') ");
+            Sql("INSERT INTO Genres (Id, Name) Values (2, 'Blues') ");
+            Sql("INSERT INTO Genres (Id, Name) Values (3, 'Rock') ");
+            Sql("INSERT INTO Genres (Id, Name) Values (4, 'Pop') ");
+            Sql("INSERT INTO Genres (Id, Name) Values (5, 'Hip-Hop') ");
+            Sql("INSERT INTO Genres (Id, Name) Values (6, 'R&B') ");
+        }
+
+        public override void Down()
+        {
+            Sql("DELETE FROM Genres WHERE Id IN (1, 2, 3, 4, 5, 6) ");
+        }
+    }
+}
